03a26a72a1fcde090f380cbe91805993e6745e71
elianmtg <elianmtoribio@gmail.com>
2022-08-15 11:41:01 -0400

    no console logs from api

--- js/app.js
+++ js/app.js
@@ -2,13 +2,10 @@ const d = document,
     $shows = d.getElementById("shows"),
     $template = d.getElementById("show-template").content,
     $fragment = d.createDocumentFragment(),
-    // $card = d.getElementById("card"),
     $btnsearch = d.getElementById("btn-search"),
     $btnload = d.getElementById("load-more"),
     $lastcard = d.querySelectorAll("container-card"),
     $loader = d.querySelector(".loader");
-    // $infoTemplate = d.getElementById("info-template");
-    // $card = d.querySelector(".container-card");
 
 let showSearch = "",
     showSearchById = "",
@@ -24,42 +21,14 @@ const getAllData = async () => {
             json = await res.json();
 
         // console.log(apiAll, res, json);
-        console.log(json);
+        // console.log(json);
         
         //si el parametro ok de la respuesta es falsa lanza el objeto
         if (!res.ok) throw { status: res.status, statusText: res.statusText }
 
-        // json.forEach(el => {
-        //     // console.log(el);
-        //     // console.log(el.show);
-        //     $template.querySelector("article").id = el.id;
-        //     // dentro del template busca el h2 y en el textContent pon el nombre del show
-        //     $template.querySelector("h2").textContent = el.name;
-        //     // si no tiene imagen le asigno una no encontrada de la misma api
-        //     $template.querySelector("img").src = el.image ? el.image.medium : "http://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
-        //     $template.querySelector("img").id = el.id;
-        //     $template.querySelector("img").alt = el.name;
-
-        //     // $template.querySelector("small").textContent = el.rating.average;
-        //     $template.querySelector("small").innerHTML = `
-        //     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-        //     xmlns="http://www.w3.org/2000/svg">
-        //         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-        //         d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 0 0 .95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 0 0-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 0 0-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 0 0-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 0 0 .951-.69l1.519-4.674z" />
-        //     </svg> ${el.rating.average ? el.rating.average : 0}`;
-
-        //     //importo el nodo template y el true es para que copie la estructura completa del template
-        //     let $clone = d.importNode($template, true);
-
-        //     //agregamos como hijo al clon
-        //     $fragment.appendChild($clone);
-
-        //     $loader.style.display = "none";
-        // });
-
         for(let index = lastShow; index <= 19; index++){
             let element = json[index];
-            console.log(element);
+            // console.log(element);
 
             $template.querySelector("article").id = element.id;
             $template.querySelector("h2").textContent = element.name;
@@ -67,6 +36,9 @@ const getAllData = async () => {
             $template.querySelector("img").src = element.image ? element.image.medium : "http://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
             $template.querySelector("img").id = element.id;
             $template.querySelector("img").alt = element.name;
+            $template.querySelector("div").id = element.id;
+            $template.querySelector("h2").id = element.id;
+            $template.querySelector("small").id = element.id;
 
             $template.querySelector("small").innerHTML = `
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -102,7 +74,7 @@ const getByParams = async (e)=> {
             res = await fetch(apiParams),
             json = await res.json();
 
-        console.log(apiParams, res, json);
+        // console.log(apiParams, res, json);
 
         //si el parametro ok de la respuesta es falsa lanza el objeto
         if (!res.ok) throw { status: res.status, statusText: res.statusText }
@@ -190,7 +162,7 @@ const getNextData = async (nextShows) => {
 
         for(let index = lastShow; index <= nextShows; index++){
             let element = json[index];
-            console.log(element);
+            // console.log(element);
 
             $template.querySelector("article").id = element.id;
             $template.querySelector("h2").textContent = element.name;
